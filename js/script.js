2ae78794176162c52e6058d267ef7eb82ebe90d4
Serhiy <example@gmail.com>
2022-05-22 17:07:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log(1);
+
+let number = 5;
+const LeftBorderWith = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
